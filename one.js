a32ca4340a9c1edd7ae7a3ecb931fcde82029895
unknown <you@example.com>
2021-03-26 22:54:26 -0500

    examen

---
+++ one.js
@@ -0,0 +1,33 @@
+
+
+function isSentenceCorrect(sentence){
+    var re = ...;
+    return re.test(sentence);
+
+}
+
+alert(isSentenceCorrect)
+
+
+// const nextLarger = (a) =>{ 
+//     const res = [];
+//     for (let i=0; i < a.length; i++){
+//     let currNum = a [i];
+//     for (let n = i; n < a.length; n++){
+//       if (a[n] > currNum){
+//          res.push(a[n]);
+//          break;
+//        } else if (n === a.length -1){
+//           res.push(-1);
+//           }
+
+//      }
+
+
+// }
+//   return res
+// } 
+
+// // nextLarger([6, 7, 3, 8])
+
+// console.log(nextLarger)
